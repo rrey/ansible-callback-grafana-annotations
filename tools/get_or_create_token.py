1e4aa1d99a86e7e6a6eda6e3eb39d99bda22b623
Rémi REY <rrey94@gmail.com>
2018-03-04 13:11:15 +0100

    tool: retrieve the grafana host info from env or use default
    If one starts the grafana container manually on a different port,
    the "make check" rule is no more working as the get_or_create_token.py
    expects the data in arguments.
    
    Now we try to get the environment variables value, if not found the
    defaults are used.

--- tools/get_or_create_token.py
+++ tools/get_or_create_token.py
@@ -1,22 +1,19 @@
 import httplib
+import os
 import sys
 import json
 import argparse
 from base64 import b64encode
 
 URI = "/api/auth/keys"
+GRAFANA_USER = os.getenv("GRAFANA_USER", "admin")
+GRAFANA_PASSWORD = os.getenv("GRAFANA_PASSWORD", "admin")
+GRAFANA_HOST = os.getenv("GRAFANA_HOST", "127.0.0.1:3000")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--user", default="admin", help="login for basic authentication")
-parser.add_argument("--passwd", default="admin", help="password for basic authentication")
-parser.add_argument("--host", default="127.0.0.1", help="Grafana host address")
-parser.add_argument("--port", default="3000", help="Grafana listen port")
-args = parser.parse_args()
-
-cred = "%s:%s" % (args.user, args.passwd)
+cred = "%s:%s" % (GRAFANA_USER, GRAFANA_PASSWORD)
 HEADERS = {"Content-Type": "application/json", "Authorization": "Basic %s" % b64encode(cred).decode('ascii')}
 
-http = httplib.HTTPConnection(args.host, args.port)
+http = httplib.HTTPConnection(GRAFANA_HOST)
 http.request("GET", URI, headers=HEADERS)
 response = http.getresponse()
 if response.status != 200:
@@ -28,14 +25,14 @@ for token in tokens:
     if token.get("name") == "ansible-module":
         token_id = token.get('id')
 if token_id:
-    http = httplib.HTTPConnection(args.host, args.port)
+    http = httplib.HTTPConnection(GRAFANA_HOST)
     http.request("DELETE", "%s/%d" % (URI, token_id), headers=HEADERS)
     response = http.getresponse()
     if response.status != 200:
         print "Failed to remove 'ansible-module' token [%s]" % response.read()
         sys.exit(1)
 
-http = httplib.HTTPConnection(args.host, args.port)
+http = httplib.HTTPConnection(GRAFANA_HOST)
 http.request("POST", URI, '{"name": "ansible-module", "role": "Editor"}', headers=HEADERS)
 response = http.getresponse()
 if response.status != 200:
